c24197a09ff5daa6e5bc9fe8935402fa1c654dd2
luis-alva0 <luisandres.alvaabanto@gmail.com>
2025-10-31 21:58:17 -0500

    fix: entrypoint new command

--- backend/docker/entrypoints/entrypoint.sh
+++ backend/docker/entrypoints/entrypoint.sh
@@ -38,5 +38,6 @@ PY
 
 python manage.py makemigrations accounts content discovery notifications tickets || true
 python manage.py migrate --noinput || true
+python manage.py collectstatic --noinput || true
 
 exec "$@"
